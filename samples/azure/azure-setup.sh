a2c798aa8eda23f45d5a84273d3ad3218c2d3430
Richard Case <richard.case@suse.com>
2024-02-15 10:48:34 +0000

    docs: updates to README
    Some updates to the readme file to use docker in the getting started
    guide.
    
    Signed-off-by: Richard Case <richard.case@suse.com>
    Signed-off-by: nasusoba <zhengrunyu2020@outlook.com>
    
    add samples and developer guide
    
    Signed-off-by: nasusoba <zhengrunyu2020@outlook.com>
    
    tiny fix

--- samples/azure/azure-setup.sh
+++ samples/azure/azure-setup.sh
@@ -1,3 +1,5 @@
+read -p "Please follow Getting Started to setup the clusterctl configuration first before running this script!"
+
 if [ -z "${AZURE_SUBSCRIPTION_ID}" ]; then
   echo "please set AZURE_SUBSCRIPTION_ID"
   exit 0
@@ -8,12 +10,10 @@ if [ -z "${AZURE_TENANT_ID}" ]; then
   exit 0
 fi
 
-
 if [ -z "${AZURE_CLIENT_ID}" ]; then
   echo "please set AZURE_CLIENT_ID"
   exit 0
 fi
-
 
 if [ -z "${AZURE_CLIENT_SECRET}" ]; then
   echo "please set AZURE_CLIENT_SECRET"
@@ -34,13 +34,6 @@ export AZURE_TENANT_ID_B64="$(echo -n "$AZURE_TENANT_ID" | base64 | tr -d '\n')"
 export AZURE_CLIENT_ID_B64="$(echo -n "$AZURE_CLIENT_ID" | base64 | tr -d '\n')"
 export AZURE_CLIENT_SECRET_B64="$(echo -n "$AZURE_CLIENT_SECRET" | base64 | tr -d '\n')"
 
-export EXP_CLUSTER_RESOURCE_SET=true
-
-
-export PWD="$(pwd)"
-mkdir -p ~/.cluster-api
-cat samples/clusterctl.yaml | envsubst > ~/.cluster-api/clusterctl.yaml
-
 clusterctl init --infrastructure azure --bootstrap k3s --control-plane k3s
 
 kubectl wait --for=condition=Available --timeout=5m -n capi-system deployment/capi-controller-manager
@@ -48,12 +41,26 @@ kubectl wait --for=condition=Available --timeout=5m -n capi-k3s-control-plane-sy
 kubectl wait --for=condition=Available --timeout=5m -n capz-system deployment/capz-controller-manager
 kubectl wait --for=condition=Available --timeout=5m -n capi-k3s-bootstrap-system deployment/capi-k3s-bootstrap-controller-manager
 
+# Settings needed for AzureClusterIdentity used by the AzureCluster
+export AZURE_CLUSTER_IDENTITY_SECRET_NAME="cluster-identity-secret"
+export CLUSTER_IDENTITY_NAME="cluster-identity"
+export AZURE_CLUSTER_IDENTITY_SECRET_NAMESPACE="default"
 
-cat samples/azure/k3s-template.yaml | envsubst > samples/azure/k3s-cluster.yaml
-kubectl create configmap azure-ccm-addon --from-file=samples/azure/azure-ccm.yaml
-kubectl create configmap azure-cn-addon --from-file=samples/azure/azure-cn.yaml
+# Create a secret to include the password of the Service Principal identity created in Azure
+# This secret will be referenced by the AzureClusterIdentity used by the AzureCluster
+kubectl create secret generic "${AZURE_CLUSTER_IDENTITY_SECRET_NAME}" --from-literal=clientSecret="${AZURE_CLIENT_SECRET}" --namespace "${AZURE_CLUSTER_IDENTITY_SECRET_NAMESPACE}"
+
+clusterctl generate cluster --from samples/azure/k3s-template.yaml $CLUSTER_NAME --kubernetes-version v1.30.2+k3s2 --worker-machine-count 2 --control-plane-machine-count 1 > samples/azure/k3s-cluster.yaml
+
 kubectl apply -f samples/azure/k3s-cluster.yaml
-kubectl apply -f samples/azure/resource-set.yaml
 
+read -p "Please wait for the cluster to be up and running, press any key to continue"
 
-echo "once the cluster is up run clusterctl get kubeconfig $CLUSTER_NAME > k3s.yaml or kubectl scale kthreescontrolplane $CLUSTER_NAME-control-plane --replicas 3 for HA"
+clusterctl get kubeconfig $CLUSTER_NAME > k3s.yaml
+
+echo "Manually apply cloud provider to workload cluster, you could visit https://cluster-api.sigs.k8s.io/user/quick-start for the latest info"
+
+# Note that the nodeSelector should be modified to fit k3s. Run this command for installing cloud provider:
+helm install --kubeconfig=./k3s.yaml --repo https://raw.githubusercontent.com/kubernetes-sigs/cloud-provider-azure/master/helm/repo cloud-provider-azure --generate-name --set infra.clusterName=$CLUSTER_NAME --set cloudControllerManager.clusterCIDR="10.42.0.0/16" --set-string cloudControllerManager.nodeSelector."node-role\.kubernetes\.io/control-plane"=true
+
+echo "You could run kubectl scale kthreescontrolplane $CLUSTER_NAME-control-plane --replicas 3 for HA"
