7e5f05a9a8998fc62ebbe43911848dc9a10e8cd0
Cannon Palms <cpalms@influxdata.com>
2024-02-26 16:01:06 -0500

    fix(controlplane): remove 2nd call to ctrl.SetupSignalHandler
    Signed-off-by: Cannon Palms <cpalms@influxdata.com>

--- controlplane/main.go
+++ controlplane/main.go
@@ -113,7 +113,7 @@ func main() {
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+	if err := mgr.Start(ctx); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
