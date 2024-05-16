7e7531a524bea62c5f8b88dfd5460121a8cdac30
Andrea Mazzotti <andrea.mazzotti@suse.com>
2024-05-16 05:36:33 +0200

    Implement CustomValidator and CustomDefaulter webhook interfaces. (#111)
    * Implement CustomValidator and CustomDefaulter webhook interfaces.
    
    Signed-off-by: Andrea Mazzotti <andrea.mazzotti@suse.com>
    
    * add default webhook
    
    Signed-off-by: nasusoba <zhengrunyu2020@outlook.com>
    
    fix webhook
    
    Signed-off-by: nasusoba <zhengrunyu2020@outlook.com>
    
    ---------
    
    Signed-off-by: Andrea Mazzotti <andrea.mazzotti@suse.com>
    Signed-off-by: nasusoba <zhengrunyu2020@outlook.com>
    Co-authored-by: nasusoba <zhengrunyu2020@outlook.com>

--- bootstrap/api/v1beta2/kthreesconfig_webhook.go
+++ bootstrap/api/v1beta2/kthreesconfig_webhook.go
@@ -17,9 +17,10 @@ limitations under the License.
 package v1beta2
 
 import (
+	"context"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
@@ -27,30 +28,33 @@ import (
 func (c *KThreesConfig) SetupWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).
 		For(c).
+		WithDefaulter(&KThreesConfig{}).
+		WithValidator(&KThreesConfig{}).
 		Complete()
 }
 
-// +kubebuilder:webhook:verbs=create;update,path=/validate-controlplane-cluster-x-k8s-io-v1beta2-kthreesconfig,mutating=false,failurePolicy=fail,matchPolicy=Equivalent,groups=bootstrap.cluster.x-k8s.io,resources=kthreesconfig,versions=v1beta2,name=validation.kthreesconfig.bootstrap.cluster.x-k8s.io,sideEffects=None,admissionReviewVersions=v1;v1beta2
-// +kubebuilder:webhook:verbs=create;update,path=/mutate-controlplane-cluster-x-k8s-io-v1beta2-kthreesconfig,mutating=true,failurePolicy=fail,matchPolicy=Equivalent,groups=bootstrap.cluster.x-k8s.io,resources=kthreesconfig,versions=v1beta2,name=default.kthreesconfig.bootstrap.cluster.x-k8s.io,sideEffects=None,admissionReviewVersions=v1;v1beta2
+// +kubebuilder:webhook:verbs=create;update,path=/validate-bootstrap-cluster-x-k8s-io-v1beta2-kthreesconfig,mutating=false,failurePolicy=fail,matchPolicy=Equivalent,groups=bootstrap.cluster.x-k8s.io,resources=kthreesconfigs,versions=v1beta2,name=validation.kthreesconfig.bootstrap.cluster.x-k8s.io,sideEffects=None,admissionReviewVersions=v1;v1beta1
+// +kubebuilder:webhook:verbs=create;update,path=/mutate-bootstrap-cluster-x-k8s-io-v1beta2-kthreesconfig,mutating=true,failurePolicy=fail,matchPolicy=Equivalent,groups=bootstrap.cluster.x-k8s.io,resources=kthreesconfigs,versions=v1beta2,name=default.kthreesconfig.bootstrap.cluster.x-k8s.io,sideEffects=None,admissionReviewVersions=v1;v1beta1
 
-var _ webhook.Defaulter = &KThreesConfig{}
-var _ webhook.Validator = &KThreesConfig{}
+var _ admission.CustomDefaulter = &KThreesConfig{}
+var _ admission.CustomValidator = &KThreesConfig{}
 
 // ValidateCreate will do any extra validation when creating a KThreesControlPlane.
-func (c *KThreesConfig) ValidateCreate() (admission.Warnings, error) {
+func (c *KThreesConfig) ValidateCreate(_ context.Context, _ runtime.Object) (admission.Warnings, error) {
 	return []string{}, nil
 }
 
 // ValidateUpdate will do any extra validation when updating a KThreesControlPlane.
-func (c *KThreesConfig) ValidateUpdate(runtime.Object) (admission.Warnings, error) {
+func (c *KThreesConfig) ValidateUpdate(_ context.Context, _, _ runtime.Object) (admission.Warnings, error) {
 	return []string{}, nil
 }
 
 // ValidateDelete allows you to add any extra validation when deleting.
-func (c *KThreesConfig) ValidateDelete() (admission.Warnings, error) {
+func (c *KThreesConfig) ValidateDelete(_ context.Context, _ runtime.Object) (admission.Warnings, error) {
 	return []string{}, nil
 }
 
 // Default will set default values for the KThreesControlPlane.
-func (c *KThreesConfig) Default() {
+func (c *KThreesConfig) Default(_ context.Context, _ runtime.Object) error {
+	return nil
 }
--- controlplane/api/v1beta2/kthreescontrolplane_webhook.go
+++ controlplane/api/v1beta2/kthreescontrolplane_webhook.go
@@ -17,9 +17,12 @@ limitations under the License.
 package v1beta2
 
 import (
+	"context"
+	"fmt"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
@@ -27,30 +30,50 @@ import (
 func (in *KThreesControlPlane) SetupWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).
 		For(in).
+		WithDefaulter(&KThreesControlPlane{}).
+		WithValidator(&KThreesControlPlane{}).
 		Complete()
 }
 
-// +kubebuilder:webhook:verbs=create;update,path=/validate-controlplane-cluster-x-k8s-io-v1beta2-kthreescontrolplane,mutating=false,failurePolicy=fail,matchPolicy=Equivalent,groups=controlplane.cluster.x-k8s.io,resources=kthreescontrolplane,versions=v1beta2,name=validation.kthreescontrolplane.controlplane.cluster.x-k8s.io,sideEffects=None,admissionReviewVersions=v1;v1beta2
-// +kubebuilder:webhook:verbs=create;update,path=/mutate-controlplane-cluster-x-k8s-io-v1beta2-kthreescontrolplane,mutating=true,failurePolicy=fail,matchPolicy=Equivalent,groups=controlplane.cluster.x-k8s.io,resources=kthreescontrolplane,versions=v1beta2,name=default.kthreescontrolplane.controlplane.cluster.x-k8s.io,sideEffects=None,admissionReviewVersions=v1;v1beta2
+// +kubebuilder:webhook:verbs=create;update,path=/validate-controlplane-cluster-x-k8s-io-v1beta2-kthreescontrolplane,mutating=false,failurePolicy=fail,matchPolicy=Equivalent,groups=controlplane.cluster.x-k8s.io,resources=kthreescontrolplanes,versions=v1beta2,name=validation.kthreescontrolplane.controlplane.cluster.x-k8s.io,sideEffects=None,admissionReviewVersions=v1;v1beta1
+// +kubebuilder:webhook:verbs=create;update,path=/mutate-controlplane-cluster-x-k8s-io-v1beta2-kthreescontrolplane,mutating=true,failurePolicy=fail,matchPolicy=Equivalent,groups=controlplane.cluster.x-k8s.io,resources=kthreescontrolplanes,versions=v1beta2,name=default.kthreescontrolplane.controlplane.cluster.x-k8s.io,sideEffects=None,admissionReviewVersions=v1;v1beta1
 
-var _ webhook.Defaulter = &KThreesControlPlane{}
-var _ webhook.Validator = &KThreesControlPlane{}
+var _ admission.CustomDefaulter = &KThreesControlPlane{}
+var _ admission.CustomValidator = &KThreesControlPlane{}
 
 // ValidateCreate will do any extra validation when creating a KThreesControlPlane.
-func (in *KThreesControlPlane) ValidateCreate() (admission.Warnings, error) {
+func (in *KThreesControlPlane) ValidateCreate(_ context.Context, _ runtime.Object) (admission.Warnings, error) {
 	return []string{}, nil
 }
 
 // ValidateUpdate will do any extra validation when updating a KThreesControlPlane.
-func (in *KThreesControlPlane) ValidateUpdate(runtime.Object) (admission.Warnings, error) {
+func (in *KThreesControlPlane) ValidateUpdate(_ context.Context, _, _ runtime.Object) (admission.Warnings, error) {
 	return []string{}, nil
 }
 
 // ValidateDelete allows you to add any extra validation when deleting.
-func (in *KThreesControlPlane) ValidateDelete() (admission.Warnings, error) {
+func (in *KThreesControlPlane) ValidateDelete(_ context.Context, _ runtime.Object) (admission.Warnings, error) {
 	return []string{}, nil
 }
 
 // Default will set default values for the KThreesControlPlane.
-func (in *KThreesControlPlane) Default() {
+func (in *KThreesControlPlane) Default(_ context.Context, obj runtime.Object) error {
+	c, ok := obj.(*KThreesControlPlane)
+	if !ok {
+		return apierrors.NewBadRequest(fmt.Sprintf("expected a KubeadmConfig but got a %T", obj))
+	}
+
+	defaultKThreesControlPlaneSpec(&c.Spec, c.Namespace)
+	return nil
+}
+
+func defaultKThreesControlPlaneSpec(s *KThreesControlPlaneSpec, namespace string) {
+	if s.Replicas == nil {
+		replicas := int32(1)
+		s.Replicas = &replicas
+	}
+
+	if s.MachineTemplate.InfrastructureRef.Namespace == "" {
+		s.MachineTemplate.InfrastructureRef.Namespace = namespace
+	}
 }
